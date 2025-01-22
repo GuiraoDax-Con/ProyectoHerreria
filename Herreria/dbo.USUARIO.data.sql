586abc4ac6faac69fbef461bf7af5937c993c50d
Jesus Gutierrez Garcia <158141202+JesusGutierrez89@users.noreply.github.com>
2025-01-22 09:19:59 +0100

    Add files via upload
    La copia del proyecto

---
+++ Herreria/dbo.USUARIO.data.sql
@@ -0,0 +1,12 @@
+﻿-- Insertar el administrador
+INSERT INTO [dbo].[USUARIO] ([nombre], [password], [rol])
+VALUES 
+('Tharivol', '1234', 'administrador');
+
+-- Insertar los usuarios
+INSERT INTO [dbo].[USUARIO] ([nombre], [password], [rol])
+VALUES 
+('Elandril', '1234', 'usuario'),
+('Morthil', '1234', 'usuario'),
+('Liafina', '1234', 'usuario'),
+('Kaelthorn', '1234', 'usuario');
