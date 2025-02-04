6c7e35dd0a6e145a3fe7cd9350d76b7f43806136
Guirao <7947346@alu365.murciaeduca.es>
2025-02-04 10:49:43 +0100

    DBO de EQUIPAMIENTO y de MATERIAS_PRIMAS_EQUIPAMIENTO

---
+++ Herreria/Herreria/asets/Scripts/DATA/dbo.MATERIAS_PRIMAS_EQUIPAMIENTO.data.sql
@@ -0,0 +1,30 @@
+﻿-- Relacionar equipamiento con materiales
+INSERT INTO [dbo].[MATERIAS_PRIMAS_EQUIPAMIENTO] (id_materia_prima, id_equipamiento, cantidad_necesaria)
+VALUES
+(7, 1, 5),  -- Espada larga de hierro
+(8, 1, 3),  -- Espada larga de acero
+(7, 2, 4),  -- Espada corta de hierro
+(8, 3, 5),  -- Hacha de batalla de acero
+(9, 3, 3),  -- Hacha de batalla de mitrhil
+(8, 4, 6),  -- Martillo de guerra de acero
+(6, 5, 3),  -- Lanza de bronce
+(5, 6, 2),  -- Daga de pieles
+(1, 7, 3),  -- Arco largo de madera
+(1, 8, 2),  -- Arco corto de madera
+(6, 9, 4),  -- Ballesta ligera de bronce
+(8, 10, 7), -- Ballesta pesada de acero
+(4, 11, 5), -- Armadura acolchada de tela
+(3, 12, 6), -- Armadura de cuero
+(7, 13, 5), -- Cota de escamas de hierro
+(7, 14, 6), -- Cota de malla de hierro
+(8, 15, 7), -- Coraza de acero
+(8, 16, 10), -- Armadura de placas de acero
+(1, 17, 4), -- Escudo de madera
+(8, 18, 6), -- Escudo de acero
+(8, 19, 5), -- Espada bastarda de acero
+(7, 20, 4), -- Maza de guerra de hierro
+(7, 21, 5), -- Pica de hierro
+(8, 22, 6), -- Espadón de acero
+(11, 23, 3), -- Daga de plata
+(7, 24, 2), -- Hacha arrojadiza de hierro
+(9, 25, 5); -- Escudo de mitrhil
